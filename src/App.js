bddc342b915d9267035c663a568db46ab9407d99
robeyoul-awal-raju <robeyoulawal@gmail.com>
2021-10-04 01:13:45 +0600

    route setup in app.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Home from './Components/Home/Home';
+import NotFound from './Components/NotfoundPage/NotFound';
+import Service from './Components/Service/Service';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/service">
+            <Service />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
